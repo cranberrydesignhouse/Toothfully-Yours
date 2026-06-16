683a17972d0aa746e83c13275e56f5bfbd14816f
cranberrydesignhouse <cranberrydesignhouse@gmail.com>
2026-06-16 18:26:14 +0530

    Update sitemap.js

--- frontend-next/app/app/app/sitemap.js
+++ frontend-next/app/app/app/sitemap.js
@@ -1,1 +1,30 @@
-
+export default function sitemap() {
+  return [
+    {
+      url: 'https://toothfullyyoursdentalcare.com',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://toothfullyyoursdentalcare.com/corrective-alignment',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://toothfullyyoursdentalcare.com/cosmetic-aesthetic-care',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://toothfullyyoursdentalcare.com/global-access',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://toothfullyyoursdentalcare.com/implants-restoration',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://toothfullyyoursdentalcare.com/neuromuscular-dentistry',
+      lastModified: new Date(),
+    }
+  ]
+}
