790b672044c9a6623e36b9ecc436a7158d58a843
cranberrydesignhouse <cranberrydesignhouse@gmail.com>
2026-06-16 18:25:17 +0530

    Update robots.js

--- frontend-next/app/app/robots.js
+++ frontend-next/app/app/robots.js
@@ -1,1 +1,11 @@
+import { MetadataRoute } from 'next'
 
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://toothfullyyoursdentalcare.com/sitemap.xml',
+  }
+}
